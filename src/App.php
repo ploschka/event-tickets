4d0929a3830c321eefa44e61f6f3f76a77dd956f
Ploshka <folga12381@gmail.com>
2024-11-02 12:35:25 +1000

    Migrations configured

--- src/App.php
+++ src/App.php
@@ -4,13 +4,17 @@ namespace App;
 
 use Doctrine\DBAL\DriverManager;
 use Doctrine\DBAL\Tools\DsnParser;
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\Configuration\Migration\ConfigurationArray;
+use Doctrine\Migrations\DependencyFactory;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\ORMSetup;
 use Symfony\Component\Dotenv\Dotenv;
 
 class App
 {
-    public $em;
+    public EntityManager $em;
+    public DependencyFactory $depFactory;
 
     private $dotenv;
 
@@ -19,14 +23,23 @@ class App
         $this->dotenv = new Dotenv();
         $this->dotenv->loadEnv("$dir/.env", null, 'prod');
 
+        $mconf = require($dir . '/config/migrations.php');
+        foreach ($mconf['migrations_paths'] as $key => $value)
+        {
+            $mconf['migrations_paths'][$key] = $dir . $value;
+        }
+
+        $migration_conf = new ConfigurationArray($mconf);
+
         $dsnParser = new DsnParser(['mysql' => 'pdo_mysql', 'postgres' => 'pdo_pgsql']);
-        $config = ORMSetup::createAttributeMetadataConfiguration(
+        $ORMconfig = ORMSetup::createAttributeMetadataConfiguration(
             paths: [$dir . '/src/Entity'],
             isDevMode: true,
         );
         $connectionParams = $dsnParser->parse($_ENV['DATABASE_URL']);
-        $connection = DriverManager::getConnection($connectionParams, $config);
-        $this->em = new EntityManager($connection, $config);
+        $connection = DriverManager::getConnection($connectionParams);
+        $this->em = new EntityManager($connection, $ORMconfig);
+        $this->depFactory = DependencyFactory::fromEntityManager($migration_conf, new ExistingEntityManager($this->em));
     }
 }
 
