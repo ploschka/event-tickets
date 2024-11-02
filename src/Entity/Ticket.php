efbd91627b14b2fb3911ad78ac2e341134485c4b
Ploshka <folga12381@gmail.com>
2024-11-02 11:37:00 +1000

    ORM mapping created

--- src/Entity/Ticket.php
+++ src/Entity/Ticket.php
@@ -3,30 +3,53 @@
 namespace App\Entity;
 
 use DateTime;
+use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Entity]
 class Ticket
 {
-    public int $id;
-    public int $event_id;
-    public DateTime $event_date;
-    public int $ticket_adult_price;
-    public int $ticket_adult_quantity;
-    public int $ticket_kid_price;
-    public int $ticket_kid_quantity;
-    public string $barcode;
-    public int $equal_price;
-    public DateTime $created;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    public ?int $id = null;
+
+    #[ORM\Column]
+    public ?int $event_id = null;
+
+    #[ORM\Column(type: "string", length: 10)]
+    public ?string $event_date = null;
+
+    #[ORM\Column]
+    public ?int $ticket_adult_price = null;
+
+    #[ORM\Column]
+    public ?int $ticket_adult_quantity = null;
+
+    #[ORM\Column]
+    public ?int $ticket_kid_price = null;
+
+    #[ORM\Column]
+    public ?int $ticket_kid_quantity = null;
+
+    #[ORM\Column(type: "string", length: 120, unique: true)]
+    public ?string $barcode = null;
+
+    #[ORM\Column]
+    public ?int $equal_price = null;
+
+    #[ORM\Column]
+    public ?DateTime $created = null;
 
     public function __construct (
         int $event_id,
-        DateTime $event_date,
+        string $event_date,
         int $ticket_adult_price,
         int $ticket_adult_quantity,
         int $ticket_kid_price,
         int $ticket_kid_quantity,
     )
     {
-        $this->$event_id = $event_id;
+        $this->event_id = $event_id;
         $this->event_date = $event_date;
         $this->ticket_adult_price = $ticket_adult_price;
         $this->ticket_adult_quantity = $ticket_adult_quantity;
